183be6fc41da9ba409e3f7e8df6c21b593acb356
Geoffrey Mainland <mainland@apeiron.net>
2017-02-13 21:07:15 -0500

    Add lecture 11 material.

---
+++ lecture11-haskell/examples.hs
@@ -0,0 +1,30 @@
+import Prelude hiding (map, filter, take, drop)
+
+fact :: Integer -> Integer
+fact = undefined
+
+fact' :: Integer -> Integer
+fact' = undefined
+
+fact'' :: Integer -> Integer
+fact'' = undefined
+
+add3 :: Int -> Int -> Int -> Int
+add3 = undefined
+
+ex16 = undefined
+
+-- Try:
+--add x = x + "String"
+
+--
+-- List operations
+--
+
+-- map :: (a -> b) -> [a] -> [b]
+
+-- append :: [a] -> [a] -> [a]
+
+-- take :: Int -> [a] -> [a]
+
+-- drop :: Int -> [a] -> [a]
